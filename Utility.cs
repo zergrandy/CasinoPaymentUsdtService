b7d222d7bd6fa743494c859b022b83c8e4ec0fdd
zergrandy <zergrandy@gmail.com>
2023-06-05 07:39:20 +0800

    init

---
+++ Utility.cs
@@ -0,0 +1,35 @@
+﻿using log4net;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace QuoteLineBot1
+{
+    public static class Utility
+    {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static void LogAndConsole(string bot, string msg)
+        {
+            msg = bot + "   " + msg;
+            log.Info(msg);
+            string dtNowStr = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
+            Console.WriteLine($"{dtNowStr}   {msg}");
+        }
+
+        public static void LogAndConsole(string msg)
+        {
+            log.Info(msg);
+            string dtNowStr = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
+            Console.WriteLine($"{dtNowStr}   {msg}");
+        }
+
+    }
+}
